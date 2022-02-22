5076af76dfeadcfa549769ba1c6adf6dc9d29278
esraa176 <72940480+esraa176@users.noreply.github.com>
2022-02-22 21:24:44 +0200

    Add files via upload

---
+++ JASON_Compiler/JASON_Compiler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TINY_Compiler
+{
+    public static class TINY_Compiler
+    {
+        public static Scanner TINY_Scanner = new Scanner();
+        public static Parser TINY_Parser = new Parser();
+        public static List<Token> TokenStream = new List<Token>();
+        public static Node treeroot;
+
+        public static void Start_Compiling(string SourceCode) //character by character
+        {
+            //Scanner
+            TINY_Scanner.StartScanning(SourceCode);
+            //Parser
+            TINY_Parser.StartParsing(TokenStream);
+            treeroot = TINY_Parser.root;
+        } 
+    }
+}
